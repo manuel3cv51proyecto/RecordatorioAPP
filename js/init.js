69305a3616e030b6e2f4e725410aa9b4b111d513
manuel3cv51proyecto <96312079+manuel3cv51proyecto@users.noreply.github.com>
2021-12-17 22:45:48 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,5 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-
+  apiKey: "AIzaSyDgy8kwdROKmfvKZSiefK0CUXRXxXlAY_E",
+  authDomain: "recordatorioapp-final.firebaseapp.com",
+  projectId: "recordatorioapp-final",
+  storageBucket: "recordatorioapp-final.appspot.com",
+  messagingSenderId: "280866493937",
+  appId: "1:280866493937:web:75f856f2475b0cf5256050",
+  measurementId: "G-3G63ETKZ2M"
 });
