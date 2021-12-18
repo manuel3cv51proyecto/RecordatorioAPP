3da186b3f3b7122a4f518da610bd1c66fa70b692
manuel3cv51proyecto <96312079+manuel3cv51proyecto@users.noreply.github.com>
2021-12-17 23:46:10 -0600

    Update CtrlSesion.js

--- js/CtrlSesion.js
+++ js/CtrlSesion.js
@@ -50,6 +50,7 @@ async function
     forma.terminarSesión.
       addEventListener(
         "click", terminaSesión);
+    window.uidPersonal = firebase.auth().currentUser.uid
   } else {
     // No ha iniciado sesión.
     iniciaSesión();
